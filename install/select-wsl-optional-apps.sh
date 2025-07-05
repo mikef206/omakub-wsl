56a909be278f9dc662c16f53840a641a756a97e6
Michael Frederickson <bmwf@gmail.com>
2025-07-05 16:41:40 -0700

    modified gcloud install

--- install/select-wsl-optional-apps.sh
+++ install/select-wsl-optional-apps.sh
@@ -6,7 +6,7 @@ for component in $components; do
 	gcloud)
 		curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
         echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-        sudo apt install google-cloud-cli -y
+        sudo apt-get update && sudo apt-get install google-cloud-cli -y
 		;;
 	Gemini)
 		npm install -g npm@11.4.2
