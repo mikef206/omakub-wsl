191a82aa5e821c970b92744355c45b38dd9de7a7
Michael Frederickson <bmwf@gmail.com>
2025-07-05 16:20:28 -0700

    Fixes to option installs

--- install/select-wsl-optional-apps.sh
+++ install/select-wsl-optional-apps.sh
@@ -1,26 +1,19 @@
 # Install optional components   
-components=$(gum choose "gcloud" "Gemini CLI" "uv" "pipx" "poetry" --no-limit --selected "gcloud" --height 9 --header "Select added components to install")
+components=$(gum choose "gcloud" "Gemini CLI" "uv" --no-limit --selected "gcloud" --height 9 --header "Select added components to install")
 
 for component in $components; do
 	case $component in
 	gcloud)
 		curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
         echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-        sudo apt-get update && sudo apt-get install google-cloud-cli
+        sudo apt install google-cloud-cli -y
 		;;
 	Gemini)
-		npm install -g @google/gemini-cli
+		npm install -g npm@11.4.2
+        npm install -g @google/gemini-cli
 		;;
 	uv)
 		wget -qO- https://astral.sh/uv/install.sh | sh
-		;;
-	pipx)
-		python3 -m pip install --user pipx
-        python3 -m pipx ensurepath
-        sudo pipx ensurepath --global # optional to allow pipx actions with --global argument
-		;;
-	poetry)
-		pipx install poetry 
 		;;
 	esac
 done
